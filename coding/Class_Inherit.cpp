e81a07192270ab4ad516822e9c1ee9cc858e3df2
wysjixiang <wysjixiang@163.com>
2022-09-01 01:18:24 +0800

    new file

---
+++ coding/Class_Inherit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// 练习类的继承 -- 编写一个Animal类作为Turtle类和Pig类的基类
+// 基类，子类的概念。 基类可以派生出其他的类，子类从基类派生而来
+
+
+
+
+
+
+
+int main()
+{
+
+    int k;
+    int a;
+    int b;
+    int c;
+
+    system("pause");
+    return 0;
+}
