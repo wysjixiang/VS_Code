b6ac7f4b3543084f29a02851aee1d8f410f32458
XIANG JI <wysjixiang@163.com>
2022-09-01 21:41:13 +0800

    Second push from lab-0901

--- coding/Class_Inherit.cpp
+++ coding/Class_Inherit.cpp
@@ -1,44 +1,97 @@
 #include <iostream>
 
-//友元关系 -如何申明友元
+//静态属性和静态函数
 
-class lover
+//声明时加上static关键字
+//static的作用: 1.可以隐藏，表示只在该文件有效
+//  2.保持变量内容的持久。存储在静态数据区的变量会在程序刚开始运行时
+//就完成初始化，并且是唯一一次初始化
+//  3.普通成员变量每个对象有各自的一份，而静态成员变量一共就一份，
+//为所有对象共享
+
+
+class Pet
 {
 public:
-    
-
-    lover(std::string name)
-    {
-        this->name = name;
-    }
+    Pet(std:: string name);
+    ~Pet();
+    int static Getcount();
 
 private:
-    std::string name;
-    friend class others;    //如果没有这个友元申明，是无法访问的！
-    //另外，如果类中的属性未非public，则无法通过classname.属性的
-    //方式访问，如lover.name。private提供了对数据的封装，使得
-    //private成员只能被类自身的成员函数以及类的友元访问，其他的
-    //函数或者类想要访问private成员只能通过该类所提供的set和get的
-    //方法进行访问
+    std:: string name;
+    static int count;
+
 };
 
+int Pet::count = 0;
 
-class others
+Pet::Pet(std:: string name)
+{
+
+    this->name = name;
+    std::cout<< name <<" borned\n"<<std::endl;
+    count++;
+}
+
+Pet::~Pet()
+{ 
+
+    std::cout<< name<<" died away\n"<<std::endl;
+    count--;
+}
+
+int Pet::Getcount()
+{
+
+    return count;
+}
+
+
+class Dog :public Pet
 {
 public:
-    void kiss(lover* lover)
-    {
-        std::cout<<"Kiss "<< lover->name <<std::endl;
-    }
+    Dog(std:: string name);
+
+};
+
+Dog::Dog(std:: string name): Pet(name)
+{
+
+};
+
+class Cat :public Pet
+{
+public:
+    Cat(std:: string name);
+
+};
+
+Cat::Cat(std:: string name): Pet(name)
+{
 
 };
 
 
 int main()
 {
-    lover girl("piggy");
-    others badguy;
-    badguy.kiss(&girl);
+
+    Dog dog("Tom");
+    Cat cat("Jerry");
+
+    std::cout<<"Now we have "<< Pet::Getcount() << " Pet(s)!"
+    <<std::endl;
+
+    {   //此处的大括号，指示了一个生命周期
+    Dog dog2("Tom2");
+    Cat cat2("Jerry2");
+
+    std::cout<<"Now we have "<< Pet::Getcount() << " Pet(s)!"
+    <<std::endl;
+    }
+
+    std::cout<<"Now we have "<< Pet::Getcount() << " Pet(s)!"
+    <<std::endl;
+
 
     system("pause");
     return 0;
