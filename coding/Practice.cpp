c032142a3bd80666a8bf270b5b4d826784e39bb4
XIANG JI <wysjixiang@163.com>
2022-09-14 21:46:45 +0800

    push from lab-0914-21:46

--- coding/Practice.cpp
+++ coding/Practice.cpp
@@ -2,65 +2,77 @@
 #include <string>
 #include <fstream>
 
-
-
-
-
-
-
-
-class Person
+template<class T>
+class Array
 {
+
 public:
-    char ZGBH[32] = {0};
-    char NAME[32] = {0};
-    char BMBH[32] = {0};
+    Array(int rom)
+    {
+        this->rom = rom;
+        this->size = 0;
+        pAddr = new T[rom];
+    }
+    Array(const Array &arr)
+    {   
+        this->rom = arr.rom;
+        this->size = arr.size;
+        this->pAddr = new T[arr.rom];
+    }
+    ~Array()
+    {
+        if(pAddr != NULL)
+        {
+
+        delete pAddr;
+        pAddr = NULL;
+
+        }
+
+    }
+
+    Array& operator= (const Array &arr)
+    {
+        if(pAddr != NULL)
+        {
+            delete pAddr;
+            pAddr = NULL;
+        } 
+
+        this->pAddr = new T[arr.rom];
+        this->rom = arr.rom;
+        this->size = arr.size;
+
+        for(int i=0;i<arr.size;i++)
+        {
+            pAddr[i] = arr.pAddr[i];
+        }
+
+        return *this;
+    }
+
+
+
+
+
+
+
+
+
+private:
+    T* pAddr;
+    int rom;
+    int size;
 
 };
 
+
+
 int main()
 {
-    Person person;
-    int i=0;
-    std::ifstream ifs;
-    ifs.open("file.csv",std::ios::in);
-    char str;
-    std::string s;
-    getline(ifs, s);
-    std::cout<< s << std::endl;
-    while( (str = ifs.get()) != '\n' && str != EOF)
-    {
-        i=0;
-        while(str != ',')
-        {
-            person.ZGBH[i++] = str;
-            str = ifs.get();
-        }
-        person.ZGBH[i] = '\0';
-        i=0;
-        while((str = ifs.get()) != ',')
-        {
-            person.NAME[i++] = str; 
-        }
-        person.NAME[i] = '\0';
-        i=0;
-        while((str = ifs.get()) != '\n')
-        {
-            person.BMBH[i++] = str;
-        }
-        person.BMBH[i] = '\0';
-    }
 
-    std::cout<< person.ZGBH << std::endl;
-    std::cout<< person.NAME << std::endl;
-    std::cout<< person.BMBH << std::endl;
 
-    ifs.close();
-    // std::ifstream ifs;
-    // ifs.open("file.csv",std::ios::in);
-    // std::string  readStr((std::istreambuf_iterator<char>(ifs)),  std::istreambuf_iterator<char>()); 
-    // std::cout<< readStr.c_str();
-    // ifs.close();
+
     system("pause");
     return 0;
 }
