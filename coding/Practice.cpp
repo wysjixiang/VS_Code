d59d4aa84136fae2963fd9d38f196cbb5b8519e4
wysjixiang <wysjixiang@163.com>
2022-09-19 23:59:46 +0800

    push from home-0919-23:59

--- coding/Practice.cpp
+++ coding/Practice.cpp
@@ -2,77 +2,27 @@
 #include <string>
 #include <fstream>
 
-template<class T>
-class Array
-{
-
-public:
-    Array(int rom)
-    {
-        this->rom = rom;
-        this->size = 0;
-        pAddr = new T[rom];
-    }
-    Array(const Array &arr)
-    {   
-        this->rom = arr.rom;
-        this->size = arr.size;
-        this->pAddr = new T[arr.rom];
-    }
-    ~Array()
-    {
-        if(pAddr != NULL)
-        {
-
-        delete pAddr;
-        pAddr = NULL;
-
-        }
-
-    }
-
-    Array& operator= (const Array &arr)
-    {
-        if(pAddr != NULL)
-        {
-            delete pAddr;
-            pAddr = NULL;
-        } 
-
-        this->pAddr = new T[arr.rom];
-        this->rom = arr.rom;
-        this->size = arr.size;
-
-        for(int i=0;i<arr.size;i++)
-        {
-            pAddr[i] = arr.pAddr[i];
-        }
-
-        return *this;
-    }
+#include <vector>
+#include <algorithm>
 
 
-
-
-
-
-
-
-
-private:
-    T* pAddr;
-    int rom;
-    int size;
-
-};
 
 
 
 int main()
 {
 
+    
+
+	std::string str;
+    str = "This is a test!";
+    std::string substr;
+    substr = str.substr(0,6);
 
 
-    system("pause");
+    std::cout << str << std::endl;
+    std::cout << substr << std::endl;
+
+    system("pause"); 
     return 0;
 }
