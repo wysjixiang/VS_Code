cb1e02124d046c12216f67300ca4112ec8cc96fe
wysjixiang <wysjixiang@163.com>
2022-09-23 02:09:17 +0800

    push from home-0923-02:09

--- coding/Practice.cpp
+++ coding/Practice.cpp
@@ -3,25 +3,48 @@
 #include <fstream>
 
 #include <vector>
+#include <deque>
 #include <algorithm>
 
+using namespace std;
 
 
 
+void printDeque(deque<int>& d) 
+{
+	for (deque<int>::iterator it = d.begin(); it != d.end(); it++) {
+		cout << *it << " ";
+
+	}
+	cout << endl;
+}
 
 int main()
 {
+    vector<int> v;
+    for(int i=0; i<10;i++)
+    {
+        v.push_back(i);
+    }
 
-    
+    deque<int> d;
+    d.assign(v.begin(),v.end());
 
-	std::string str;
-    str = "This is a test!";
-    std::string substr;
-    substr = str.substr(0,6);
+    deque<int>::iterator p = d.begin();
+    while(p!=d.end())
+    {
+        cout<< *(p++) <<endl;
+    }
 
+    d.insert(d.begin()+1,d.begin(),p);
+    sort(d.begin(),d.end());
+    reverse(d.begin(),d.end());
 
-    std::cout << str << std::endl;
-    std::cout << substr << std::endl;
+    p = d.begin();
+    while(p!=d.end())
+    {
+        cout<< *(p++) <<endl;
+    }
 
     system("pause"); 
     return 0;
